b3a85b1db3e453074205c30cbc5fd0ee692d7f72
ikede-divine-keno <ikededivine@gmail.com>
2023-09-22 11:49:03 -0700

    0-create_a_file.pp modified

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,7 +4,7 @@ File { '/tmp/school':
   ensure => file,
   path => '/tmp/school',
   mode => '0744',
-  owner => www-data,
-  group=> www-data,
+  owner => 'www-data',
+  group=> 'www-data',
   content => 'I love Puppet'
 }
