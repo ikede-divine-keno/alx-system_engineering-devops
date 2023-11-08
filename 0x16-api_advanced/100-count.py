d54eecc566d83f14dc1eb86fba0ef7a258468c8a
ikede-divine-keno <ikededivine@gmail.com>
2023-11-07 16:38:03 -0800

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -19,11 +19,14 @@ def count_words(subreddit, word_list, hot_list=[], after="", count=0):
     if response.status_code != 200:
         return None
 
-    results = response.json()["data"]
-    after = results["after"]
-    count += results["dist"]
-    for c in results["children"]:
-        hot_list.append(c["data"]["title"])
+    try:
+        results = response.json()["data"]
+        after = results["after"]
+        count += results["dist"]
+        for c in results["children"]:
+            hot_list.append(c["data"]["title"])
+    except ValueError:
+        return None
 
     if after is None:
         try:
@@ -34,7 +37,7 @@ def count_words(subreddit, word_list, hot_list=[], after="", count=0):
                     if key in wrd:
                         wrd[key] += 1
             srd = sorted(wrd.items(), key=lambda item: (-item[1], item[0]))
-            [print("{}: {}".format(key, val)) for key, val in srd if val]
+            [print("{}: {}".format(key, val)) for key, val in srd if val > 0]
             return hot_list
         except [ValueError, TypeError, KeyError,
                 IndexError, AttributeError]:
