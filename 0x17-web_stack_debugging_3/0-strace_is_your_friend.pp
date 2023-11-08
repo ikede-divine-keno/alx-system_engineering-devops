13ba7fce02db89a48b3677169e5cadd50a2e8bf7
ikede-divine-keno <ikededivine@gmail.com>
2023-11-08 09:17:47 -0800

    strace 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet script that automates the fixing of bad extension '.phpp' to '.php' in the WordPress site file 'wp-settings.php'
 
 exec { 'Fix-WordPress-site-settings':
-  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  provider=> shell
+  command => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider=> shell,
 }
